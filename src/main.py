94cdac1acf26111f59b483ca067761a730793880
Guruk90 <daniel.i.hansson@gmail.com>
2025-01-30 01:44:18 +0100

    test

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from textnode import *
 
 
 def main():
-    test_node = TextNode("silly", TextType.BOLD,"https://www.boot.dev")
+    test_node = TextNode("silly me", TextType.BOLD,"https://www.boot.dev")
     print(test_node)
 
 
